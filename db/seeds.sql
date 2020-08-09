57792210aad832a9055fbfab834272493d572074
Jared Varnum <jvarnum3@gmail.com>
2020-08-08 19:31:36 -0500

    adding in databases, and establishing file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('TEST');
+INSERT INTO burgers (name) VALUES ('TEST');
+INSERT INTO burgers (name, devoured) VALUES ('BOOLEAN', true);
+INSERT INTO burgers (name, devoured) VALUES ('BOOLEAN', true);
+INSERT INTO burgers (name, devoured) VALUES ('BOOLEAN', true);
+INSERT INTO burgers (name) VALUES ('TEST');
